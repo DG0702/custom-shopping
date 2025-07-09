104f2d8c3ac6e7909d2e39f62d87de871e325f50
Jindnjs <wlsdnjs0310@naver.com>
2025-07-10 00:33:11 +0900

    feat: 공통 페이지 응답 객체 추가

---
+++ src/main/java/com/example/shopping/domain/common/dto/PageResponseDto.java
@@ -0,0 +1,25 @@
+package com.example.shopping.domain.common.dto;
+
+import lombok.Getter;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
+@Getter
+public class PageResponseDto<T> {
+
+    private final List<T> content;
+    private final long totalElements;
+    private final int totalPages;
+    private final int size;
+    private final int number;
+
+    public PageResponseDto(Page<T> page){
+        content = page.getContent();
+        totalElements = page.getTotalElements();
+        totalPages = page.getTotalPages();
+        size = page.getSize();
+        number = page.getNumber();
+    }
+
+}
