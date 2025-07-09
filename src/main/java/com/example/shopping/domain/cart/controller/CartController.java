c50bae6d4631c8a8814c8a00a7ddeec0d2d95c9e
Jindnjs <wlsdnjs0310@naver.com>
2025-07-09 17:26:28 +0900

    feat: 장바구니 추가 공통 응답 처리

--- src/main/java/com/example/shopping/domain/cart/controller/CartController.java
+++ src/main/java/com/example/shopping/domain/cart/controller/CartController.java
@@ -3,7 +3,9 @@ package com.example.shopping.domain.cart.controller;
 import com.example.shopping.domain.cart.dto.CartCreateRequestDto;
 import com.example.shopping.domain.cart.service.CartService;
 import com.example.shopping.domain.common.dto.AuthUser;
+import com.example.shopping.domain.common.dto.ResponseDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,11 +19,12 @@ public class CartController {
     private final CartService cartService;
 
     @PostMapping("/api/cart")
-    public String create(
+    public ResponseEntity<ResponseDto<Void>> create(
         @RequestBody @Validated CartCreateRequestDto request,
         @AuthenticationPrincipal AuthUser user
     ){
         cartService.create(request, user.getId());
-        return "장바구니에 상품이 추가되었습니다.";
+        ResponseDto<Void> responseDto = new ResponseDto<>("장바구니에 상품이 추가되었습니다.", null);
+        return ResponseEntity.ok(responseDto);
     }
 }
