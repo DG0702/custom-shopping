b282c225358193fc1365fb2f26af56d7b6e34d08
DongGeun <kk506kk@naver.com>
2025-07-09 12:02:34 +0900

    refactor: price, totalPrice 정의

--- src/main/java/com/example/shopping/domain/order/common/OrderMapper.java
+++ src/main/java/com/example/shopping/domain/order/common/OrderMapper.java
@@ -1,7 +1,6 @@
 package com.example.shopping.domain.order.common;
 
 import com.example.shopping.domain.cart.dto.OrderItemRequest;
-import com.example.shopping.domain.order.dto.OrderRequestDto;
 import com.example.shopping.domain.order.entity.Order;
 import com.example.shopping.domain.order.entity.OrderItem;
 import com.example.shopping.domain.order.enums.OrderStatus;
@@ -11,19 +10,21 @@ import com.example.shopping.domain.user.entity.User;
 public class OrderMapper {
 
     // order Entity
-    public static Order order(User user, OrderRequestDto dto){
+    public static Order order(User user, Long totalPrice){
+
         return Order.builder()
                 .user(user)
+                .totalPrice(totalPrice)
                 .status(OrderStatus.ORDERED)
                 .build();
     }
 
-    public static OrderItem orderItem(Order order, Product product, OrderRequestDto dto, OrderItemRequest itemDto){
+    public static OrderItem orderItem(Order order, Product product, OrderItemRequest itemDto){
 
         return OrderItem.builder()
                 .order(order)
                 .product(product)
-                .price()
+                .price(product.getPrice())
                 .quantity(itemDto.getQuantity())
                 .build();
     }
