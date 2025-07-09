439ca3bfe10f7e0c4879cb29132ed405a6269636
Jindnjs <wlsdnjs0310@naver.com>
2025-07-09 20:29:06 +0900

    feat: QueryDsl 설정 추가, Qrepository 정의

---
+++ src/main/java/com/example/shopping/domain/cart/repository/QCartRepositoryImpl.java
@@ -0,0 +1,8 @@
+package com.example.shopping.domain.cart.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class QCartRepositoryImpl implements QCartRepository {
+
+}
