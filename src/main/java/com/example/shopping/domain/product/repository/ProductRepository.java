cbacb0158d0f83503dec70802792bae29b955236
DongGeun <kk506kk@naver.com>
2025-07-14 15:40:49 +0900

    feat : 재고차감, 재고복구 NativeQuery를 이용해 생성

--- src/main/java/com/example/shopping/domain/product/repository/ProductRepository.java
+++ src/main/java/com/example/shopping/domain/product/repository/ProductRepository.java
@@ -5,6 +5,7 @@ import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -20,4 +21,14 @@ public interface ProductRepository extends JpaRepository<Product,Long> {
     //상품 목록 조회
     @Query("SELECT p FROM Product p ORDER BY p.id ASC")
     Page<Product> findAllProductPaged(Pageable pageable);
+
+    // 재고 차감
+    @Modifying
+    @Query(value = "UPDATE products SET stock = stock - :quantity WHERE id = :productId AND stock >= :quantity", nativeQuery = true)
+    int decreaseStock(@Param("productId") Long productId, @Param("quantity") Integer quantity);
+
+    // 재고 복구
+    @Modifying
+    @Query(value = "UPDATE products SET stock = stock + :quantity WHERE id = :productId", nativeQuery = true)
+    void increaseStock (@Param("productId") Long productId, @Param("quantity") Integer quantity);
 }
