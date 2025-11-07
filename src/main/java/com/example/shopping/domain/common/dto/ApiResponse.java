1c63d419aa1beb973f35fc04ba154bc81a689d51
DongGeun <kk506kk@naver.com>
2025-11-07 19:47:50 +0900

    feat : API 공통 응답 처리

---
+++ src/main/java/com/example/shopping/domain/common/dto/ApiResponse.java
@@ -0,0 +1,40 @@
+package com.example.shopping.domain.common.dto;
+
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class ApiResponse<T> {
+    private boolean success;
+    private String message;
+    private T data;
+    private LocalDateTime timestamp;
+
+    // message, data(ResponseDto) 이용해서 공통응답 생성
+    public ApiResponse(String message, T data) {
+        this.success = true;
+        this.message = message;
+        this.data = data;
+        this.timestamp = LocalDateTime.now();
+    }
+
+    private ApiResponse(boolean success, String message, T data) {
+        this.success = success;
+        this.message = message;
+        this.data = data;
+        this.timestamp = LocalDateTime.now();
+    }
+
+    public static <T> ApiResponse<T> success(String message, T data) {
+        return new ApiResponse<>(true, message, data);
+    }
+
+    public static <T> ApiResponse<T> error(String message) {
+        return new ApiResponse<>(false, message, null);
+    }
+
+    public static <T> ApiResponse<T> error(String message, T data) {
+        return new ApiResponse<>(false, message, data);
+    }
+}
