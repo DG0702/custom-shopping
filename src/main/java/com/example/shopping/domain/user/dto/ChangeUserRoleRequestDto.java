c3bcdbde5feba39c828709021690fc62e1468915
DongGeun <kk506kk@naver.com>
2025-11-10 14:54:05 +0900

    refactor : 생성자 정리

--- src/main/java/com/example/shopping/domain/user/dto/ChangeUserRoleRequestDto.java
+++ src/main/java/com/example/shopping/domain/user/dto/ChangeUserRoleRequestDto.java
@@ -1,14 +1,12 @@
 package com.example.shopping.domain.user.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class ChangeUserRoleRequestDto {
     @NotBlank(message = "유저 권한은 필수입니다")
     private final String userRole;
-
-    public ChangeUserRoleRequestDto(String userRole){
-        this.userRole = userRole;
-    }
 }
