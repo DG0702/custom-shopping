588726f1fd07e8b7eca5a5f8d9431d579159763e
DongGeun <kk506kk@naver.com>
2025-11-12 17:40:19 +0900

    remove : 불필요한 Class,Interface 삭제

--- src/main/java/com/example/shopping/domain/product/repository/JdbcProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.shopping.domain.product.repository;
-
-import com.example.shopping.domain.product.dto.ViewCountUpdateDto;
-
-import java.util.List;
-
-public interface JdbcProductRepository {
-    void batchUpdateDailyViewCount(List<ViewCountUpdateDto> updateList);
-}
