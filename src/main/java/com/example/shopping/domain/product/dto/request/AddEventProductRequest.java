570a26ed71ce75cd5ccab9e4170b0ec11634246b
DongGeun <kk506kk@naver.com>
2025-11-13 09:41:13 +0900

    refactor : 어노테이션 추가하여 필드 검증

--- src/main/java/com/example/shopping/domain/product/dto/request/AddEventProductRequest.java
+++ src/main/java/com/example/shopping/domain/product/dto/request/AddEventProductRequest.java
@@ -1,22 +1,29 @@
 package com.example.shopping.domain.product.dto.request;
 
+import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
+import com.example.shopping.domain.product.annotation.EndDate;
+
 @Getter
 @AllArgsConstructor
-@NoArgsConstructor
-public class AddEventProductRequestDto {
+@EndDate
+public class AddEventProductRequest {
+
     @NotNull
     private Integer eventPrice;
+
     @NotNull
     private Integer eventStock;
+
     @NotNull
+    @FutureOrPresent(message = "시작 시간은 현재 시각 이후여야 합니다.")
     private LocalDateTime start;
+
     @NotNull
     private LocalDateTime end;
 }
