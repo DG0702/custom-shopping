7a9e244c6418ce1a4dbb54c458c9735c5db5740a
DongGeun <kk506kk@naver.com>
2025-11-13 15:21:16 +0900

    refactor : CartController 코드 통일

--- src/main/java/com/example/shopping/domain/cart/controller/CartController.java
+++ src/main/java/com/example/shopping/domain/cart/controller/CartController.java
@@ -1,52 +1,57 @@
 package com.example.shopping.domain.cart.controller;
 
-import com.example.shopping.domain.cart.dto.CartCreateRequestDto;
-import com.example.shopping.domain.cart.dto.CartResponseDto;
+import com.example.shopping.domain.cart.dto.CartCreateRequest;
+import com.example.shopping.domain.cart.dto.CartResponse;
 import com.example.shopping.domain.cart.service.CartService;
-import com.example.shopping.domain.common.dto.AuthUser;
-import com.example.shopping.domain.common.dto.PageResponseDto;
-import com.example.shopping.domain.common.dto.ResponseDto;
+import com.example.shopping.global.common.dto.ApiResponse;
+
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/cart")
 public class CartController {
 
     private final CartService cartService;
 
-    @PostMapping("/api/cart")
-    public ResponseEntity<ResponseDto<Void>> create(
-        @RequestBody @Validated CartCreateRequestDto request,
-        @AuthenticationPrincipal AuthUser user
-    ){
-        cartService.create(request, user.getId());
-        ResponseDto<Void> responseDto = new ResponseDto<>("장바구니에 상품이 추가되었습니다.", null);
-        return ResponseEntity.status(HttpStatus.CREATED).body(responseDto);
+    // 장바구니 상품 추가
+    @PostMapping()
+    public ResponseEntity<ApiResponse<Void>> create(
+        @RequestBody @Valid CartCreateRequest request,
+        @AuthenticationPrincipal Long userId
+    ) {
+        cartService.create(request, userId);
+        return ResponseEntity.status(HttpStatus.CREATED)
+            .body(ApiResponse.success("장바구니에 상품이 추가", null));
     }
 
-    @GetMapping("/api/cart")
-    public ResponseEntity<ResponseDto<PageResponseDto<CartResponseDto>>> getCarts(
-        @AuthenticationPrincipal AuthUser user,
+    // 장바구니 목록 조회
+    @GetMapping()
+    public ResponseEntity<ApiResponse<Page<CartResponse>>> getCarts(
+        @AuthenticationPrincipal Long userId,
         Pageable pageable
-    ){
-        PageResponseDto<CartResponseDto> page = cartService.getPage(user.getId(), pageable);
-        ResponseDto<PageResponseDto<CartResponseDto>> response = new ResponseDto<>("장바구니 목록을 조회하였습니다.", page);
-        return ResponseEntity.ok(response);
+    ) {
+        Page<CartResponse> response = cartService.getAllCartItems(userId, pageable);
+        return ResponseEntity.status(HttpStatus.OK)
+            .body(ApiResponse.success("장바구니 목록 조회", response));
     }
 
-    @DeleteMapping("/api/cart/{cartId}")
-    public ResponseEntity<ResponseDto<Void>> delete(
-            @PathVariable Long cartId,
-            @AuthenticationPrincipal AuthUser user
-    ){
-        cartService.delete(user.getId(), cartId);
-        ResponseDto<Void> responseDto = new ResponseDto<>("장바구니 항목이 삭제되었습니다.", null);
-        return ResponseEntity.status(HttpStatus.OK).body(responseDto);
+    // 장바구니 상품 삭제
+    @DeleteMapping("{cartId}")
+    public ResponseEntity<ApiResponse<Void>> delete(
+        @PathVariable Long cartId,
+        @AuthenticationPrincipal Long userId
+    ) {
+        cartService.delete(userId, cartId);
+        return ResponseEntity.status(HttpStatus.OK)
+            .body(ApiResponse.success("장바구니 항목이 삭제", null));
     }
 }
