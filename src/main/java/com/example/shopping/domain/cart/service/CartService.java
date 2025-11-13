196223e264935693f6b0b5db50e252182f28f023
DongGeun <kk506kk@naver.com>
2025-11-13 15:23:45 +0900

    refactor : cart 비즈니스 계층 수정

--- src/main/java/com/example/shopping/domain/cart/service/CartService.java
+++ src/main/java/com/example/shopping/domain/cart/service/CartService.java
@@ -1,18 +1,19 @@
 package com.example.shopping.domain.cart.service;
 
-import com.example.shopping.domain.cart.dto.CartCreateRequestDto;
-import com.example.shopping.domain.cart.dto.CartResponseDto;
+import com.example.shopping.domain.cart.dto.CartCreateRequest;
+import com.example.shopping.domain.cart.dto.CartResponse;
 import com.example.shopping.domain.cart.entity.CartItem;
 import com.example.shopping.domain.cart.repository.CartRepository;
-import com.example.shopping.domain.common.dto.PageResponseDto;
-import com.example.shopping.domain.common.exception.CustomException;
-import com.example.shopping.domain.common.exception.ExceptionCode;
+import com.example.shopping.global.common.exception.CustomException;
+import com.example.shopping.global.common.exception.ErrorCode;
 import com.example.shopping.domain.product.entity.Product;
 import com.example.shopping.domain.product.service.ProductService;
 import com.example.shopping.domain.user.entity.User;
 import com.example.shopping.domain.user.service.UserQueryService;
+
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -20,13 +21,13 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class CartService {
-    private final CartRepository cartRepository;
 
+    private final CartRepository cartRepository;
     private final UserQueryService userQueryService;
     private final ProductService productService;
 
     @Transactional
-    public void create(CartCreateRequestDto request, Long userId) {
+    public void create(CartCreateRequest request, Long userId) {
         User user = userQueryService.findByIdOrElseThrow(userId);
         Product product = productService.findByIdOrElseThrow(request.getProductId());
 
@@ -35,18 +36,19 @@ public class CartService {
     }
 
     @Transactional
-    public PageResponseDto<CartResponseDto> getPage(Long userId, Pageable pageable) {
+    public Page<CartResponse> getAllCartItems(Long userId, Pageable pageable) {
         userQueryService.findByIdOrElseThrow(userId);
-        Page<CartResponseDto> paged = cartRepository.getCartPage(userId, pageable);
-        return new PageResponseDto<>(paged);
+        return cartRepository.getCartPage(userId, pageable);
+
     }
 
     @Transactional
     public void delete(Long userId, Long cartId) {
         CartItem cartItem = cartRepository.findById(cartId)
-                .orElseThrow(()-> new CustomException(ExceptionCode.CART_NOT_FOUND));
-        if(!cartItem.getUser().getId().equals(userId)) {
-            throw new CustomException(ExceptionCode.FORBIDDEN);
+            .orElseThrow(() -> new CustomException(ErrorCode.CART_NOT_FOUND));
+
+        if (!cartItem.getUser().getId().equals(userId)) {
+            throw new CustomException(ErrorCode.ACCESS_DENIED);
         }
         cartRepository.delete(cartItem);
     }
