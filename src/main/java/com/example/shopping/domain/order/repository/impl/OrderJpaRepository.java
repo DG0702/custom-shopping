2caa88afce54abb9da6d828492512d019f8fd1d2
DongGeun <kk506kk@naver.com>
2025-11-19 15:25:34 +0900

    refactor : 불필요한 메서드 삭제

---
+++ src/main/java/com/example/shopping/domain/order/repository/impl/OrderJpaRepository.java
@@ -0,0 +1,8 @@
+package com.example.shopping.domain.order.repository.impl;
+
+import com.example.shopping.domain.order.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+// Order 사용할 Repository
+public interface OrderJpaRepository extends JpaRepository<Order, Long> {
+}
