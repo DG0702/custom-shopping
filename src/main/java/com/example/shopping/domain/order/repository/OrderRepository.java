a03fd9076d22339591c4881ae4262617df7bafba
DongGeun <kk506kk@naver.com>
2025-11-19 15:27:09 +0900

    remove : 불필요한 메서드 삭제

--- src/main/java/com/example/shopping/domain/order/repository/OrderRepository.java
+++ src/main/java/com/example/shopping/domain/order/repository/OrderRepository.java
@@ -1,16 +1,15 @@
 package com.example.shopping.domain.order.repository;
 
-import com.example.shopping.domain.common.dto.AuthUser;
-import com.example.shopping.domain.order.dto.OrderCancelDto;
-import com.example.shopping.domain.order.dto.OrderItemListDto;
-import com.example.shopping.domain.order.dto.OrderResponseDto;
+import com.example.shopping.domain.order.dto.orderResponse.OrderItemResponse;
+import com.example.shopping.domain.user.entity.User;
+import com.example.shopping.domain.order.dto.OrderCancel;
+import com.example.shopping.domain.order.dto.orderResponse.OrderResponse;
 import com.example.shopping.domain.order.entity.Order;
-import com.example.shopping.domain.order.enums.OrderStatus;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 import java.util.List;
-
 
 /**
  * OrderJpaRepository + OrderItemJpaRepository + QueryDslRepository 를 함께 사용 할 수 있는 공통 인터페이스
@@ -21,13 +20,9 @@ public interface OrderRepository {
 
     Order save(Order order);
 
-    Page<OrderResponseDto> getOrders(AuthUser authUser, Pageable pageable);
+    Page<OrderResponse> getOrders(User user, Pageable pageable);
 
-    Page<OrderItemListDto> getOrderItems(Order order, Pageable pageable);
+    OrderItemResponse getOrderItems(Long orderId);
 
-    List<OrderCancelDto> orderCancel(Long orderId);
-
-    OrderStatus findByOrderStatus(Long orderId);
-
-    void changeStatus(Long orderId, String status);
+    List<OrderCancel> orderCancel(Long orderId);
 }
---
+++ src/main/java/com/example/shopping/domain/order/repository/impl/OrderRepositoryImpl.java
@@ -0,0 +1,54 @@
+package com.example.shopping.domain.order.repository.impl;
+
+import com.example.shopping.domain.order.dto.orderResponse.OrderItemResponse;
+import com.example.shopping.domain.order.repository.OrderRepository;
+import com.example.shopping.domain.user.entity.User;
+import com.example.shopping.global.common.exception.CustomException;
+import com.example.shopping.global.common.exception.ErrorCode;
+import com.example.shopping.domain.order.dto.OrderCancel;
+import com.example.shopping.domain.order.dto.orderResponse.OrderResponse;
+import com.example.shopping.domain.order.entity.Order;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+
+// OrderRepository 구현체
+public class OrderRepositoryImpl implements OrderRepository {
+
+    private final OrderJpaRepository orderjpaRepository;
+    private final OrderRepositoryQueryDSL orderRepositoryQueryDSL;
+
+    @Override
+    public Order findById(Long id) {
+        return orderjpaRepository.findById(id).
+            orElseThrow(() -> new CustomException(ErrorCode.ORDER_NOT_FOUND));
+    }
+
+    @Override
+    public Order save(Order order) {
+        return orderjpaRepository.save(order);
+    }
+
+    @Override
+    public Page<OrderResponse> getOrders(User user, Pageable pageable) {
+        return orderRepositoryQueryDSL.getOrders(user, pageable);
+    }
+
+    @Override
+    public OrderItemResponse getOrderItems(Long orderId) {
+        return orderRepositoryQueryDSL.getOrderItems(orderId);
+    }
+
+    @Override
+    public List<OrderCancel> orderCancel(Long orderId) {
+        return orderRepositoryQueryDSL.orderCancel(orderId);
+    }
+}
