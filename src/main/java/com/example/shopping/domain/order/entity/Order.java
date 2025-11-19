5bcd012e79b9d62a44deaae016bfb4c8e976ea18
DongGeun <kk506kk@naver.com>
2025-11-19 09:06:16 +0900

    refactor : Order, OrderItem 양방향 연관관계에서 단방향 연관관계로 수정 후 Dirty Checking으로 update 되도록 구현

--- src/main/java/com/example/shopping/domain/order/entity/Order.java
+++ src/main/java/com/example/shopping/domain/order/entity/Order.java
@@ -1,17 +1,16 @@
 package com.example.shopping.domain.order.entity;
 
-import com.example.shopping.domain.common.entity.TimeStamped;
+import com.example.shopping.global.common.entity.BaseEntity;
 import com.example.shopping.domain.order.enums.OrderStatus;
 import com.example.shopping.domain.user.entity.User;
+
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.util.ArrayList;
 import java.util.List;
-
 
 @Getter
 @Entity
@@ -19,7 +18,7 @@ import java.util.List;
 @Table(name = "orders")
 @AllArgsConstructor
 @Builder
-public class Order extends TimeStamped {
+public class Order extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -28,32 +27,26 @@ public class Order extends TimeStamped {
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
-    // @Builder가 필드의 초기화를 무시하여 무시하도록 하기 위해 설정
-    @Builder.Default
-    @OneToMany (mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<OrderItem> orderItems = new ArrayList<>();
-
     private Integer totalPrice;
 
     @Enumerated(EnumType.STRING)
     private OrderStatus status;
 
-    // Order 저장 시 OrderItem 저장하기 위한 메서드
-    public void addOrderItem(OrderItem orderItem) {
-        orderItems.add(orderItem);
-        orderItem.setOrder(this);
-    }
-
-    public static Order createOrder(User user){
+    public static Order createOrder(User user) {
         Order order = new Order();
         order.user = user;
         order.status = OrderStatus.ORDERED;
         return order;
     }
-    public void updateTotalPrice(){
-        this.totalPrice = orderItems.stream()
-                .mapToInt(OrderItem::getPriceWithQuantity)
-                .sum();
+
+    public void updateTotalPrice(List<OrderItem> purchasedItems) {
+        this.totalPrice = purchasedItems.stream()
+            .mapToInt(OrderItem::getPriceWithQuantity)
+            .sum();
+    }
+
+    public void updateStatus() {
+        this.status = OrderStatus.CANCELED;
     }
 
 }
