8a23f5d8b763e7b514b1624621cbb4724a426046
DongGeun <kk506kk@naver.com>
2025-11-26 16:24:03 +0900

    refactor : 테스트 할 때 필요한 기본 데이터 클래스 수정

--- src/test/java/com/example/shopping/domain/order/util/TestDataFactory.java
+++ src/test/java/com/example/shopping/domain/order/util/TestDataFactory.java
@@ -1,16 +1,16 @@
 package com.example.shopping.domain.order.util;
 
-import com.example.shopping.domain.cart.dto.CartCreateRequestDto;
-import com.example.shopping.domain.order.dto.OrderRequestDto;
+import com.example.shopping.domain.cart.entity.CartItem;
 import com.example.shopping.domain.product.entity.Product;
 import com.example.shopping.domain.product.repository.ProductRepository;
 import com.example.shopping.domain.user.entity.User;
 import com.example.shopping.domain.user.enums.UserRole;
 import com.example.shopping.domain.user.repository.UserRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.test.util.ReflectionTestUtils;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
@@ -22,59 +22,23 @@ public class TestDataFactory {
     @Autowired
     private ProductRepository productRepository;
 
-    private User testUser;
-
-    private Product testProduct;
-
-    /**
-     * 테스트 시작 전 상품 1개, 재고 100개 저장
-     * @PostConstruct : 테스트 시작 전 초기화 (한 번만 실행)
-     */
-    //@PostConstruct
-    public void init(){
-
-        User user = User.builder()
-                .email("test@example.com")
-                .password("1234")
-                .name("testUser")
-                .address("testAddress")
-                .userRole(UserRole.USER)
-                .build();
-
-        testUser = userRepository.save(user);
-
-        Product product = Product.builder()
-                    .name("상품")
-                    .price(1000)
-                    .stock(100)
-                    .build();
-
-        testProduct = productRepository.save(product);
+    public User createUser(String email, String password, String name, String address) {
+        return userRepository.save(new User(email, password, name, address, UserRole.USER));
     }
 
-    // 유저 반환
-    public User getTestUser(){
-        return testUser;
+    public Product createProduct(String name, String description, Integer price, Integer stock) {
+        return productRepository.save(new Product(name, description, price, stock));
     }
 
-    // 상품 반환
-    public Product getTestProduct(){
-        return testProduct;
+    public List<CartItem> cartItems(User user, List<Product> products, int quantity) {
+        List<CartItem> cartItems = new ArrayList<>();
+
+        for (Product product : products) {
+            CartItem cartItem = CartItem.createCartItem(user, product, quantity);
+            cartItems.add(cartItem);
+        }
+
+        return cartItems;
     }
 
-    // 주문 생성할 dto 생성 → 요청 값(@RequestBody)
-    public OrderRequestDto orderRequestDto (Long productId, Integer quantity){
-        // 카트 안에 객체 생성(카트 안에 1개의 상품만 넣음)
-        CartCreateRequestDto item = new CartCreateRequestDto(productId,quantity);
-
-        List<CartCreateRequestDto> items = List.of(item);
-
-        // 빈 객체 생성
-        OrderRequestDto dto = new OrderRequestDto();
-
-        // OrderRequestDto 객체 생성 (객체, 필드명, 값)
-        ReflectionTestUtils.setField(dto,"items", items);
-
-        return dto;
-    }
 }
